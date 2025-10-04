08771b609d8c931d00f1bdaf40ac76cb4b0d5263
ohad12345678 <ohadbanay4@gmail.com>
2025-10-05 01:02:59 +0300

    delete

--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 ﻿from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-from fastapi.middleware.cors import CORSMiddleware
+from fastapi.middleware.cors  CORSMiddleware
 import os
 
 app = FastAPI()
