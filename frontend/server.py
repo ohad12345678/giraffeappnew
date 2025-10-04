d57d835d6da6b95041e7cd1ba7d27365fcf94752
ohad12345678 <ohadbanay4@gmail.com>
2025-10-04 11:50:48 +0300

    Add files via upload

---
+++ frontend/server.py
@@ -0,0 +1,16 @@
+﻿import http.server
+import socketserver
+import os
+
+PORT = int(os.environ.get("PORT", 8080))
+
+class MyHTTPRequestHandler(http.server.SimpleHTTPRequestHandler):
+    def end_headers(self):
+        self.send_header('Cache-Control', 'no-store, no-cache, must-revalidate')
+        super().end_headers()
+
+Handler = MyHTTPRequestHandler
+
+with socketserver.TCPServer(("", PORT), Handler) as httpd:
+    print(f"Server running on port {PORT}")
+    httpd.serve_forever()
