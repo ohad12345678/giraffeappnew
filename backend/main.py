30f3b19224f305f45490b2f5063052ab4e957539
ohad12345678 <ohadbanay4@gmail.com>
2025-10-05 01:03:27 +0300

    Update main.py

--- backend/main.py
+++ backend/main.py
@@ -1,6 +1,6 @@
 from fastapi import FastAPI
 from fastapi.responses import HTMLResponse
-from fastapi..cors import CORSMiddleware
+from fastapi.middleware.cors import CORSMiddleware
 import os
 
 app = FastAPI()
